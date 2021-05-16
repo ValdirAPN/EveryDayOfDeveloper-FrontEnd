b0708c5f9589ffd66681164a437725c13f4ecef7
valdirpn <valdiraires.pn@gmail.com>
2021-05-16 00:06:53 -0300

    chore: theme settings

---
+++ src/styles/theme.ts
@@ -0,0 +1,38 @@
+export default {
+  grid: {
+    container: '117.6rem',
+    gutter: '2.4rem'
+  },
+  border: {
+    radius: '0.5rem'
+  },
+  font: {
+    family:
+      "Sora,--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    normal: 400,
+    bold: 700,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '2rem',
+      xlarge: '2.4rem',
+      xxlarge: '5.6rem'
+    }
+  },
+  colors: {
+    primary: '#E961FF',
+    secondary: '#6C63FF',
+    mainBg: '#2F3034',
+    lightGray: '#9A9A9A',
+    gray: '#3D3D3D',
+    white: '#FFFFFF'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+}
